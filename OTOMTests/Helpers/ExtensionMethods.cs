5990f1bb57d4d58c9a381d623651b08a80999179
avais88786 <avaisuddin.mohammad@outlook.com>
2015-02-04 16:47:35 +0000

    Wednesday Afternoon

--- OTOMTests/Helpers/ExtensionMethods.cs
+++ OTOMTests/Helpers/ExtensionMethods.cs
@@ -16,13 +16,43 @@ namespace OTOMTests.Helpers
         }
 
 
-        public static int MaxRepeatGroupValue<TModel, TType>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TType>> expression,string propertyName)
+        public static int MaxRepeatGroupValue<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
         {
+            Type typee = helper.ViewData.ModelMetadata.ModelType;
+            var propertyMetaData = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+            string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+            //var y = ExpressionHelper.GetExpressionText(expression);
             //var x = ((MaximumPropertyRepeatGroupsAttribute)expression.Body.Type.GetCustomAttributes(typeof(MaximumPropertyRepeatGroupsAttribute), false).SingleOrDefault()).MaxPRGValue;
 
-            var attr = ((MaximumPropertyRepeatGroupsAttribute)expression.Parameters[0].Type.GetProperty(propertyName).GetCustomAttributes(typeof(MaximumPropertyRepeatGroupsAttribute), false).SingleOrDefault()).MaxPRGValue;
+            var attr = ((MaximumPropertyRepeatGroupsAttribute)propertyMetaData.ContainerType.GetProperty(propertyMetaData.PropertyName).GetCustomAttributes(typeof(MaximumPropertyRepeatGroupsAttribute), false).SingleOrDefault()).MaxPRGValue;
             //expression.
             return attr;
         }
+
+
+        #region example
+        //public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, object htmlAttributes)
+        //{
+        //    return LabelFor(html, expression, new RouteValueDictionary(htmlAttributes));
+        //}
+
+        //public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, IDictionary<string, object> htmlAttributes)
+        //{
+        //    ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+        //    string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+        //    string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+        //    if (String.IsNullOrEmpty(labelText))
+        //    {
+        //        return MvcHtmlString.Empty;
+        //    }
+
+        //    TagBuilder tag = new TagBuilder("label");
+        //    tag.MergeAttributes(htmlAttributes);
+        //    tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+        //    tag.SetInnerText(labelText);
+        //    return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        //}
+
+        #endregion
     }
 }
---
+++ OTOMTests/Scripts/CustomScripts/PropertyRepeatGroup.js
@@ -0,0 +1,85 @@
+﻿$(document).ready(function () {
+
+    
+    HideProperties();
+
+    function HideProperties() {
+        
+        $("#RepeatGroupContainer > div").hide();
+        $("#PropertyRepeatGroup_0").show();
+        //var maxProperties = $("#MaxPropertyRepeatValue").val();
+
+        //for (var i = 1; i < maxProperties; i++) {
+        //    $("#PropertyRepeatGroup_" + i).hide();
+        //}
+
+        //$("#RepeatGroupContainer_Tabs > div > div").hide();
+
+        var maxProperties = $("#MaxPropertyRepeatValue").val();
+
+        for (var i = 2; i <= maxProperties; i++) {
+            $("#repeatGroupTab_" + i).hide();
+        }
+
+
+    };
+
+
+    $("#addProperty").click(function () {
+
+        var maxProperties = $("#MaxPropertyRepeatValue").val();
+        var currentId = $("#currentPropertyRepeatGroupId").val();
+        currentId = ++currentId;
+        var repeatGroup = $("#PropertyRepeatGroup_" + currentId);//.show(500);//.slideDown();
+
+        if (repeatGroup.length) //if element exists
+        {
+            repeatGroup.show(500);
+            $("#currentPropertyRepeatGroupId").val(currentId);
+            $("#removeProperty").removeAttr("hidden");
+        }
+        
+        if (currentId >= maxProperties-1)
+            $(this).hide();
+        
+
+    });
+
+
+    $("#removeProperty").click(function () {
+
+        var currentId = $("#currentPropertyRepeatGroupId").val();
+        var repeatGroup = $("#PropertyRepeatGroup_" + currentId);//.hide(500);//.slideDown();
+
+        if (repeatGroup.length) {
+            repeatGroup.hide(500);
+            currentId = --currentId;
+            $("#currentPropertyRepeatGroupId").val(currentId);
+            
+        }
+        
+        if (currentId == 0)
+            $(this).attr("hidden", "hidden");
+        else
+        {
+            $("#addProperty").show();
+        }
+
+    });
+
+
+    $("#addProperty_Tab").click(function () {
+        var currentId = $("#currentPropertyRepeatGroupId_Tab").val();
+        $("#tabBody_" + currentId).hide(500);
+        currentId = ++currentId;
+        var repeatGroup = $("#tabBody_" + currentId);
+
+        if (repeatGroup.length) {
+            repeatGroup.show(500);
+            $("#currentPropertyRepeatGroupId_Tab").val(currentId);
+        }
+
+    });
+
+
+});
